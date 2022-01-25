203925bc600782cb89f79f55691d8f05b8834880
Mohamed Nabassoua <mnabassoua@gmail.com>
2022-01-25 18:31:06 -0500

    intro to variables

---
+++ Intro/variables.c
@@ -0,0 +1,21 @@
+/*Les variables*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+short a, b, c;
+
+int main(int argc, char *argv[]){
+
+  short d, e, f;
+
+  printf("&a:%p \t &b:%p \t &c:%p \n", &a, &b, &c);
+  printf("&d:%p \t &e:%p \t &f:%p \n", &d, &e, &f);
+
+  return EXIT_SUCCESS;
+
+}
+   
+  
+
+  
